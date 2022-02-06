7c9d8983cd4d514e4f4aa49bd7c73903da01a820
Vym38224 <jardaavymetal@gmail.com>
2022-02-06 10:36:46 +0100

    upravadoma

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-
 from os.path import basename, splitext
-import tkinter as tk
+import re
 import random
+import tkinter as tk
 from tkinter import *
 
 # from tkinter import ttk
@@ -13,70 +12,73 @@ class Application(tk.Tk):
     name = "Matematika"
 
     def __init__(self):
+
         super().__init__(className=self.name)
+        self.lbl = tk.Label(self, text="Matematika")
         self.title(self.name)
-
         self.bind("<Escape>", self.quit)
-
-        self.lbl = tk.Label(self, text="Vypočítej:")
-        self.lbl.pack()
-
-    
-        #výsledek
-        self.entry = Entry(self, width = 5)
-        self.entry.pack(side = RIGHT,padx= 40, pady=20)
-
-        #=
-        self.label1 = Label(self, text ="=")
-        self.label1.pack(side = RIGHT)
-
-        #příklad
-        self.entry = Entry(self, width = 15)
-        self.entry.pack(side = RIGHT, padx= 40, pady=20)
-
-        #self.btn = tk.Button(self, text="Quit", command=self.quit)
-        #self.btn.pack()
-
+        self.lbl.grid(row=0)
+        self.lblVl = tk.Label(self, text="")
+        self.lblVl.grid(row=3, column=2)
         self.generuj()
-        
+        self.entry = Entry(self, width=5)
+        self.entry.grid(row=2, column=3)
+        self.bind('<Return>', self.kontrola)
+        self.btn = tk.Button(self, text="Konec", command=self.quit)
+        self.btn.grid(row=3, column=3)
+       
 
+
+    def kontrola(self, event):
+        try:
+            if int(self.entry.get()) == self.vysledek:
+                self.lblVl.config(text="SPRÁVNĚ", fg="green")
+
+            else:
+                self.lblVl.config(text="ŠPATNĚ",fg="red")
+
+            self.generuj()
+            self.entry.delete(0, END)
+        except ValueError:
+            return
 
     def generuj(self):
-        self.funkce = random.choice([self.plus, self.deleno, self.krat, self.minus])
-        self.label = Label(self, text=1)
-        
+        self.funkce = random.choice([self.plus, self.deleno, self.minus, self.krat])
+        self.priklad = self.funkce()
+        self.lbl.config(text=self.priklad)
+        self.lbl.grid(row=2, column=2)
 
     def plus(self):
         self.cisloA = random.randint(1, 99)
-        self.cisloB = random.randint(1, 100-self.cisloA) 
+        self.cisloB = random.randint(1, 100 - self.cisloA)
         self.vysledek = self.cisloA + self.cisloB
-        self.lbl.config(text="+")
+        self.znamenko = "+"
+        return f"{self.cisloA}{self.znamenko}{self.cisloB} ="
 
     def deleno(self):
         self.cisloA = random.randint(1, 10)
-        self.vysledek =random.randint(1, 10)
-        self.cisloB = self.cisloB * self.vysledek
-        self.lbl.config(text="/")
+        self.vysledek = random.randint(1, 10)
+        self.cisloB = self.cisloA * self.vysledek
+        self.znamenko = "/"
+        return f"{self.cisloB}{self.znamenko}{self.cisloA} ="
 
     def minus(self):
         self.cisloA = random.randint(1, 99)
-        self.cisloB = random.randint(1, self.cisloA) 
+        self.cisloB = random.randint(1, self.cisloA)
         self.vysledek = self.cisloA - self.cisloB
-        self.lbl.config(text="-")
+        self.znamenko = "-"
+        return f"{self.cisloA}{self.znamenko}{self.cisloB} ="
 
     def krat(self):
         self.cisloA = random.randint(1, 10)
-        self.cisloB = random.randint(1, 10) 
+        self.cisloB = random.randint(1, 10)
         self.vysledek = self.cisloA * self.cisloB
-        self.lbl.config(text="*")   
-
-    def about(self):
-        window = About(self)
-        window.grab_set()
+        self.znamenko = "*"
+        return f"{self.cisloA}{self.znamenko}{self.cisloB} ="
 
     def quit(self, event=None):
         super().quit()
 
 
 app = Application()
-app.mainloop()
+app.mainloop() 
